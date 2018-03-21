6a272afb660e9824b8a22a8ee6770480deafe69d
Marvin Danig <marvin@marvindanig.com>
2018-03-21 17:00:07 -0400

    Added a coin/balance wrapper over the blockchain

--- sikka.js
+++ sikka.js
@@ -1,15 +1,22 @@
 const SHA256 = require('crypto-js/sha256')
 
+class Transaction {
+	constructor (fromAddress, toAddress, amount) {
+		this.fromAddress = fromAddress
+		this.toAddress = toAddress
+		this.amount = amount
+	}
+}
+
 class Block {
-		constructor (index, timeStamp, data, prevHash = '') {
-			this.index = index
+		constructor (timeStamp, transactions, prevHash = '') {
 			this.timeStamp = timeStamp
-			this.data = data
+			this.transactions = transactions
 			this.prevHash = prevHash
 			this.hash = this.calculateHash()
 			this.nonce = 0;
 		}
-		calculateHash () { return SHA256(this.index + this.prevHash + this.timeStamp + JSON.stringify(this.data) + this.nonce).toString() }
+		calculateHash () { return SHA256(this.prevHash + this.timeStamp + JSON.stringify(this.transactions) + this.nonce).toString() }
 		mineBlock (difficulty) {
 			while ( this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
 				this.nonce++;
@@ -23,20 +30,39 @@ class BlockChain {
 	constructor () {
 		this.chain = [this.createGenesisBlock()]
 		this.isValid = false
-		this.difficulty = 6 // Takes about a minute to execute on 2GB RAM
+		this.difficulty = 2 // Takes about a minute to execute on 2GB RAM
+		this.pendingTransactions = []
+		this.miningReward = 100
 	}
 	createGenesisBlock (){
-		return new Block(0, Date.now(), "Genesis block", "0")
+		return new Block(Date.now(), "Genesis block", "0")
 	}
 	getLatestBlock(){
 		return this.chain[this.chain.length - 1]
 	}
-	addBlock(newBlock) {
-		newBlock.prevHash = this.getLatestBlock().hash
-		newBlock.mineBlock(this.difficulty)
-		this.chain.push(newBlock)
+	getBalanceOfAddress(address){
+		let balance = 0;
+		for (const block of this.chain) {
+			for (const trans of block.transactions) {
+				if (trans.fromAddress === address) balance -= trans.amount
+				if (trans.toAddress === address) balance += trans.amount
+			}
+		}
+		return balance
+	}
+	minePendingTransactions(miningRewardAddress){
+		let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash)
+		block.mineBlock(this.difficulty)
+		console.log('Block mined successfully.', block)
+		this.chain.push(block)
+		this.pendingTransactions = [ new Transaction(null, miningRewardAddress, this.miningReward)]
 		this.isValid = this.isChainValid()
 	}
+
+	createTransaction(transaction){
+		this.pendingTransactions.push(transaction)
+	}
+
 	isChainValid(){
 		for(let i = 1; i < this.chain.length; i++) {
 			const currentBlock = this.chain[i]
@@ -49,12 +75,32 @@ class BlockChain {
 }
 
 let sikka = new BlockChain()
-console.log('Mining first block…')
-sikka.addBlock(new Block(1, Date.now(), { amount: 10 } ))
 
-console.log('Mining second block…')
-sikka.addBlock(new Block(1, Date.now(), { amount: 15 } ))
+
+sikka.createTransaction(new Transaction('address1', 'address2', 100))
+sikka.createTransaction(new Transaction('address2', 'address1', 50))
+
+console.log('\n Starting the miner')
+
+sikka.minePendingTransactions('minersAddress1')
+
+console.log('\n Balance of minersAddress1 is =', sikka.getBalanceOfAddress('minersAddress1'))
+
+// // console.log('Mining first block…')
+// sikka.addBlock(new Block( Date.now(), { amount: 10 } ))
+
+// // console.log('Mining second block…')
+// // sikka.addBlock(new Block( Date.now(), { amount: 15 } ))
+
+
+
+sikka.createTransaction(new Transaction('address3', 'address2', 50))
+sikka.createTransaction(new Transaction('address1', 'address3', 20))
+
+console.log('\n Starting the miner')
+
+sikka.minePendingTransactions('minersAddress1')
+
+console.log('\n Balance of minersAddress1 is =', sikka.getBalanceOfAddress('minersAddress1'))
 
 console.log(JSON.stringify(sikka, null, 4))
-
-
